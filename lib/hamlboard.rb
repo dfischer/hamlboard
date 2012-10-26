4fbdb3b5664f49909a4f4e4667183546263b4453
DFischer <me@danielfischer.com>
2012-10-26 15:31:31 -0700

    Hamlboard

--- lib/hamlboard.rb
+++ lib/hamlboard.rb
@@ -1,15 +1,2 @@
 #!/usr/bin/env ruby
 require "hamlboard/version"
-require 'main'
-
-module Hamlboard
-  Main {
-    def run
-      system("pbpaste > ~/.html")
-      system("html2haml ~/.html > ~/.haml")
-      system("pbcopy < ~/.haml")
-      p "Hamlized version in clipboard!"
-      exit_success!
-    end
-  }
-end
